f0b60a794399e347b2d3bc1c184ccc411bc6a138
Pringled <thomas123@live.nl>
2024-09-24 15:08:19 +0200

    Moved template to file

---
+++ assets/model_card_template.md
@@ -0,0 +1,63 @@
+---
+model_name: {model_name}
+base_model: {base_model}
+library_name: 'model2vec'
+license: {license}
+tags: [embeddings, sentence-transformers, static-embeddings]
+---
+
+# {model_name} Model Card
+
+Model2Vec distills a Sentence Transformer into a small, static model.
+This model is ideal for applications requiring fast, lightweight embeddings.
+
+
+
+## Installation
+
+Install model2vec using pip:
+```
+pip install model2vec
+```
+
+## Usage
+A StaticModel can be loaded using the `from_pretrained` method:
+```python
+from model2vec import StaticModel
+model = StaticModel.from_pretrained("minishlab/M2V_base_output")
+embeddings = model.encode(["Example sentence"])
+```
+
+Alternatively, you can distill your own model using the `distill` method:
+```python
+from model2vec.distill import distill
+
+# Choose a Sentence Transformer model
+model_name = "BAAI/bge-base-en-v1.5"
+
+# Distill the model
+m2v_model = distill(model_name=model_name, pca_dims=256)
+
+# Save the model
+m2v_model.save_pretrained("m2v_model")
+```
+
+## How it works
+
+Model2vec creates a small, fast, and powerful model that outperforms other static embedding models by a large margin on all tasks we could find, while being much faster to create than traditional static embedding models such as GloVe. Best of all, you don't need any data to distill a model using Model2Vec.
+
+It works by passing a vocabulary through a sentence transformer model, then reducing the dimensionality of the resulting embeddings using PCA, and finally weighting the embeddings using zipf weighting. During inference, we simply take the mean of all token embeddings occurring in a sentence.
+
+## Citation
+
+Please cite the [Model2Vec repository](https://github.com/MinishLab/model2vec) if you use this model in your work.
+
+## Additional Resources
+
+- [Model2Vec Repo](https://github.com/MinishLab/model2vec)
+- [Model2Vec Results](https://github.com/MinishLab/model2vec?tab=readme-ov-file#results)
+- [Model2Vec Tutorials](https://github.com/MinishLab/model2vec/tree/main/tutorials)
+
+## Model Authors
+
+Model2Vec was developed by the [Minish Lab](https://github.com/MinishLab) team consisting of Stephan Tulkens and Thomas van Dongen.
--- model2vec/utils.py
+++ model2vec/utils.py
@@ -86,85 +86,23 @@ def _create_model_card(folder_path: Path, **kwargs: Any) -> None:
         fOut.write(model_card)
 
 
-def _generate_model_card(model_name: str, **kwargs: Any) -> str:
+def _generate_model_card(model_name: str, template_path: str = "assets/model_card_template.md", **kwargs: Any) -> str:
     """
     Generate a model card using a string template.
 
     :param model_name: The name of the model.
+    :param template_path: The path to the template file.
     :param **kwargs: Additional metadata for the model card (e.g. language, base_model, etc.).
     :return: The content of the model card in markdown format.
     """
-    # Retrieve metadata from kwargs
-    base_model = kwargs.get("base_model_name", "unknown")
-    language = kwargs.get("language", "unknown")
-    license = kwargs.get("license", "mit")
+    # Load the template
+    with open(template_path, "r") as file:
+        template_content = file.read()
 
-    # Define the content for the model card
-    model_card_content = f"""
----
-model_name: {model_name}
-base_model: {base_model}
-library_name: 'model2vec'
-license: {license}
-tags: [embeddings, sentence-transformers, static-embeddings]
----
-
-# {model_name} Model Card
-
-Model2Vec distills a Sentence Transformer into a small, static model.
-This model is ideal for applications requiring fast, lightweight embeddings.
-
-
-
-## Installation
-
-Install model2vec using pip:
-```
-pip install model2vec
-```
-
-## Usage
-A StaticModel can be loaded using the `from_pretrained` method:
-```python
-from model2vec import StaticModel
-model = StaticModel.from_pretrained("minishlab/M2V_base_output")
-embeddings = model.encode(["Example sentence"])
-```
-
-Alternatively, you can distill your own model using the `distill` method:
-```python
-from model2vec.distill import distill
-
-# Choose a Sentence Transformer model
-model_name = "BAAI/bge-base-en-v1.5"
-
-# Distill the model
-m2v_model = distill(model_name=model_name, pca_dims=256)
-
-# Save the model
-m2v_model.save_pretrained("m2v_model")
-```
-
-## How it works
-
-Model2vec creates a small, fast, and powerful model that outperforms other static embedding models by a large margin on all tasks we could find, while being much faster to create than traditional static embedding models such as GloVe. Best of all, you don't need any data to distill a model using Model2Vec.
-
-It works by passing a vocabulary through a sentence transformer model, then reducing the dimensionality of the resulting embeddings using PCA, and finally weighting the embeddings using zipf weighting. During inference, we simply take the mean of all token embeddings occurring in a sentence.
-
-## Citation
-
-Please cite the Model2Vec repository if you use this model in your work.
-
-## Additional Resources
-
-- [Model2Vec Repo](https://github.com/MinishLab/model2vec)
-- [Model2Vec Results](https://github.com/MinishLab/model2vec?tab=readme-ov-file#results)
-- [Model2Vec Tutorials](https://github.com/MinishLab/model2vec/tree/main/tutorials)
-
-## Model Authors
-
-Model2Vec was developed by the [Minish Lab](https://github.com/MinishLab) team consisting of Stephan Tulkens and Thomas van Dongen.
-    """
+    # # Fill in the placeholders in the template
+    model_card_content = template_content.format(
+        model_name=model_name, base_model=kwargs.get("base_model_name", "unknown"), license=kwargs.get("license", "mit")
+    )
 
     return model_card_content
 
