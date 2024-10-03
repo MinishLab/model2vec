a78c9e3fe16a8f17ca81f62f6fc3c6c1c1d29000
stephantul <stephantul@gmail.com>
2024-10-03 16:15:41 +0200

    enh: Add device selection mechanism

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from tempfile import NamedTemporaryFile
+from unittest.mock import patch
 
+import pytest
+
+from model2vec.distill.utils import select_optimal_device
 from model2vec.utils import _get_metadata_from_readme
 
 
@@ -23,3 +27,26 @@ def test__get_metadata_from_readme_mocked_file_keys() -> None:
         f.write(b"")
         f.flush()
         assert set(_get_metadata_from_readme(Path(f.name))) == set()
+
+
+@pytest.mark.parametrize(
+    "device, expected, cuda, mps",
+    [
+        ("cpu", "cpu", True, True),
+        ("cpu", "cpu", True, False),
+        ("cpu", "cpu", False, True),
+        ("cpu", "cpu", False, False),
+        ("clown", "clown", False, False),
+        (None, "cuda", True, True),
+        (None, "cuda", True, False),
+        (None, "mps", False, True),
+        (None, "cpu", False, False),
+    ],
+)
+def test_select_optimal_device(device: str | None, expected: str, cuda: bool, mps: bool) -> None:
+    """Test whether the optimal device is selected."""
+    with (
+        patch("torch.cuda.is_available", return_value=cuda),
+        patch("torch.backends.mps.is_available", return_value=mps),
+    ):
+        assert select_optimal_device(device) == expected
