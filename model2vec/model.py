6e5234200ada96f4159c8edfb7bd9587ec7bee56
Stephan Tulkens <stephantul@gmail.com>
2025-01-15 20:51:53 +0100

    feat: Add loading from st (#151)
    * Add ability to load from sentence-transformers
    * Remove legacy loading

--- model2vec/model.py
+++ model2vec/model.py
@@ -160,11 +160,32 @@ class StaticModel:
         """
         from model2vec.hf_utils import load_pretrained
 
-        embeddings, tokenizer, config, metadata = load_pretrained(path, token=token)
+        embeddings, tokenizer, config, metadata = load_pretrained(path, token=token, from_sentence_transformers=False)
 
         return cls(
             embeddings, tokenizer, config, base_model_name=metadata.get("base_model"), language=metadata.get("language")
         )
+
+    @classmethod
+    def from_sentence_transformers(
+        cls: type[StaticModel],
+        path: PathLike,
+        token: str | None = None,
+    ) -> StaticModel:
+        """
+        Load a StaticModel trained with sentence transformers from a local path or huggingface hub path.
+
+        NOTE: if you load a private model from the huggingface hub, you need to pass a token.
+
+        :param path: The path to load your static model from.
+        :param token: The huggingface token to use.
+        :return: A StaticModel
+        """
+        from model2vec.hf_utils import load_pretrained
+
+        embeddings, tokenizer, config, _ = load_pretrained(path, token=token, from_sentence_transformers=True)
+
+        return cls(embeddings, tokenizer, config, base_model_name=None, language=None)
 
     def encode_as_sequence(
         self,
