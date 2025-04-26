fcd1ebdad3f78362851f4243bd16cb6880567de5
stephantul <stephantul@gmail.com>
2025-04-26 18:57:35 +0200

    wip

--- model2vec/distill/tokenizer.py
+++ model2vec/distill/tokenizer.py
@@ -5,32 +5,6 @@ import logging
 from typing import Any
 
 from tokenizers import Tokenizer
-from tokenizers.pre_tokenizers import (
-    BertPreTokenizer,
-    ByteLevel,
-    CharDelimiterSplit,
-    Digits,
-    Metaspace,
-    PreTokenizer,
-    Punctuation,
-    Sequence,
-    Split,
-    UnicodeScripts,
-    Whitespace,
-    WhitespaceSplit,
-)
-
-_FORBIDDEN_PRETOKENIZERS = (
-    BertPreTokenizer,
-    CharDelimiterSplit,
-    Metaspace,
-    Punctuation,
-    Split,
-    UnicodeScripts,
-    Whitespace,
-    WhitespaceSplit,
-)
-
 
 from model2vec.distill.utils import Token
 
@@ -64,9 +38,9 @@ def _pre_tokenize_vocabulary(tokenizer: Tokenizer, tokens: list[Token]) -> list[
             if token.is_subword:
                 pre_tokenized_tokens.append(token.form)
             else:
-                # Join tokens just to be sure.
+                # We know 100% sure that all pretokenized tokens will have length 1.
                 pretokenized_tokens, _ = zip(*tokenizer.pre_tokenizer.pre_tokenize_str(f" {token.form}"))
-                pre_tokenized_tokens.append(" ".join(pretokenized_tokens))
+                pre_tokenized_tokens.append(pretokenized_tokens[-1])
     else:
         pre_tokenized_tokens = [token.form for token in tokens]
 
@@ -93,30 +67,6 @@ def _remap_added_tokens(
         token["id"] = vocabulary.index(token["content"])
 
     return special_tokens
-
-
-def _fix_single_pretokenizer(pretokenizer: PreTokenizer) -> PreTokenizer | None:
-    """Fixes a single pretokenizer to allow multiword units."""
-    if isinstance(pretokenizer, _FORBIDDEN_PRETOKENIZERS):
-        return Metaspace(split=False, replacement="Ġ")
-    elif isinstance(pretokenizer, ByteLevel):
-        pretokenizer.use_regex = False
-
-    return pretokenizer
-
-
-def _fix_pretokenizer_for_super(pre: PreTokenizer | None) -> Tokenizer:
-    """Fixes the pretokenizer to allow multiword units."""
-    if pre is None:
-        return pre
-
-    if isinstance(pre, Sequence):
-        new_pretokenizers = []
-        for pretokenizer in pre:
-            new_pretokenizers.append(_fix_single_pretokenizer(pretokenizer))
-        return Sequence(new_pretokenizers)
-
-    return _fix_single_pretokenizer(pre)
 
 
 def _make_new_merges_from_vocab(
@@ -161,7 +111,6 @@ def replace_vocabulary(
     tokenizer: Tokenizer, new_vocabulary: list[Token], unk_token: str | None, pad_token: str | None
 ) -> Tokenizer:
     """Replace the vocabulary of a tokenizer with a new one."""
-    tokenizer.pre_tokenizer = _fix_pretokenizer_for_super(tokenizer.pre_tokenizer)
     tokenizer_json: dict[str, Any] = json.loads(tokenizer.to_str())
 
     # NOTE: all tokens have been normalized before.
@@ -177,30 +126,10 @@ def replace_vocabulary(
         unk_token = unk_token or tokenizer_json["model"]["unk_token"]
         tokenizer_json["model"]["unk_token"] = unk_token
         tokenizer_json["added_tokens"] = [x for x in tokenizer_json["added_tokens"] if x["content"] in special_tokens]
-
-        if model_type == "WordPiece":
-            subword_prefix = tokenizer_json["model"]["continuing_subword_prefix"]
-            new_vocab = {}
-            for idx, token in enumerate(pre_tokenized_tokens):
-                if token in special_tokens:
-                    # We need to remove the prefix from the token
-                    pass
-                elif token.startswith(subword_prefix):
-                    # We need to remove the prefix from the token
-                    token = token.removeprefix(subword_prefix)
-                elif token.startswith("Ġ"):
-                    pass
-                else:
-                    # We need to add the prefix to the token
-                    token = f"Ġ{token}"
-                new_vocab[token] = idx
-            tokenizer_json["model"]["continuing_subword_prefix"] = ""
-            tokenizer_json["model"]["max_input_chars_per_word"] = 10_000
-            tokenizer_json["model"]["vocab"] = new_vocab
+        tokenizer_json["model"]["vocab"] = {token: idx for idx, token in enumerate(pre_tokenized_tokens)}
 
         if model_type == "BPE":
             # Bit more difficult, we need to take into account merges.
-            tokenizer_json["model"]["vocab"] = {token: idx for idx, token in enumerate(pre_tokenized_tokens)}
             merges = tokenizer_json["model"]["merges"]
             merges = _make_new_merges_from_vocab(merges, pre_tokenized_tokens, special_tokens)
             tokenizer_json["model"]["merges"] = merges
@@ -227,5 +156,4 @@ def replace_vocabulary(
     tokenizer_json["added_tokens"] = _remap_added_tokens(added_tokens, pre_tokenized_tokens)
     tokenizer_json["post_processor"] = _DEFAULT_POST_PROCESSOR_TEMPLATE
 
-    tokenizer = Tokenizer.from_str(json.dumps(tokenizer_json))
-    return tokenizer
+    return Tokenizer.from_str(json.dumps(tokenizer_json))
